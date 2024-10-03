7eb17985b8f7c9d6980bec020e21c813d6d5569a
Tyler Estell <108036777+Testell@users.noreply.github.com>
2024-10-03 22:06:43 +0000

    need to split expired and active

--- app/models/board.rb
+++ app/models/board.rb
@@ -8,4 +8,13 @@
 #  updated_at :datetime         not null
 #
 class Board < ApplicationRecord
+  has_many(:posts, class_name: "Post", foreign_key: "board_id")
+
+  def posts
+    post_id = self.id
+
+    matching_posts = Post.where({ :board_id => post_id})
+
+    return matching_posts
+  end
 end
